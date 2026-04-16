39412c6aeaf36848f55dc99df67241538b8ca528
samAlcazar <alcazar.samuel@gmail.com>
2026-04-16 01:19:15 -0400

    feat: update database configuration to use environment variables exclusively

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import { Pool } from 'pg'
 
 const DBconnect = {
-  user: process.env.DB_USER || 'ceadl',
+  user: process.env.DB_USER,
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'ceadl',
-  password: process.env.DB_PASSWORD || 'mi4v-aee3-5939',
-  port: process.env.DB_PORT || 5432
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT) || 5432
 }
 
 export const pool = new Pool(DBconnect)
